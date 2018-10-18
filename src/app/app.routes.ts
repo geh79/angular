5a785b0f81799ae6578918bed17e50f1c29f7550
geh <germaneherrera@gmail.com>
2018-10-18 18:17:14 -0500

    Ajusta parametros artista

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@
 import { RouterModule, Routes } from '@angular/router';
 import { LanzamientosComponent } from './components/lanzamientos/lanzamientos.component';
 import { HomeComponent } from './components/home/home.component';
+import { ArtistaComponent } from './components/artista/artista.component';
 
 const APP_ROUTES: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'lanzamientos', component: LanzamientosComponent },
+  { path: 'artista/:id', component: ArtistaComponent },
   { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
--- src/app/components/artista/artista.component.ts
+++ src/app/components/artista/artista.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-artista',
@@ -7,7 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArtistaComponent implements OnInit {
 
-  constructor() { }
+  private artista:any = {};
+
+  constructor(
+     private activatedRoute: ActivatedRoute
+  ) {
+    this.activatedRoute.params.subscribe( params =>{
+      console.log(params['id']);
+        // this.artista = this._heroesService.getHeroe( params['id'] );
+        // console.log(this.heroe);
+    });
+
+   }
+
+
 
   ngOnInit() {
   }
