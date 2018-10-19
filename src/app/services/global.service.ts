c2246b508e23f4363eb94c1f0c03048d4b92ae46
geh <germaneherrera@gmail.com>
2018-10-18 21:18:37 -0500

    Menor

--- src/app/services/global.service.ts
+++ src/app/services/global.service.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
 export class GlobalService {
 
   private urlApi:string = "https://api.spotify.com/v1/";
-  private authorization:string = 'Bearer BQBbO0QlkACDcpTrkVCvFEJvsA73vFGT-FG2vsA4oQ8Nsv-U4dTkukdkC1ortKoMyw4B9Bpsttp241DxweM';
+  private authorization:string = 'Bearer BQCET8VmfNmloo5h2W01W-QAbSjYU1A55qjHQenoJcDBNfQStKm3qc1vGCPdyMiQCcQe7WpVIsOzxBp2g8I';
 
   constructor() { }
 
