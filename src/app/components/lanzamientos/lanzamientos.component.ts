fadd3c0f1c6b15ae2b9ea43e59018b6e2a8574db
geh <germaneherrera@gmail.com>
2018-10-18 21:18:05 -0500

    Ajusta funcionalidad lanzamientos

--- src/app/components/lanzamientos/lanzamientos.component.ts
+++ src/app/components/lanzamientos/lanzamientos.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SolicitudesService } from '../../services/solicitudes.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-lanzamientos',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LanzamientosComponent implements OnInit {
 
-  constructor() { }
+  dataLanzamientos:any = {};
+
+  constructor(
+    private solicitudes:SolicitudesService,
+    private router: Router
+  ) {
+    this.ultimosLanzamientos();
+   }
 
   ngOnInit() {
   }
+
+  private ultimosLanzamientos(){
+    this.solicitudes.ultimosLanzamientos().subscribe(res=>{
+        this.dataLanzamientos = res;
+        this.dataLanzamientos = this.dataLanzamientos.albums.items;        
+    });
+  }
+
+  public cargarartista(item:any){
+    this.router.navigate([ '/artista', item.id  ]);
+  }
+
 
 }
